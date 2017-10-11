999918c88464d503b197b7ebe15b097e8e83f9e5
Michael Kelley <mikelley@mines.edu>
2017-10-11 16:33:19 -0600

    Completed coin flip action

--- JavaScriptGame/game.js
+++ JavaScriptGame/game.js
@@ -2,6 +2,9 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var name = "";
+var toss = "";
+var coin = 0;
+var userBet = false;
 
 // This is the initial function when the game begins
 $(document).ready(function() {
@@ -44,13 +47,13 @@ $(document).ready(function() {
 			$("#nameInput").css("visibility", "hidden");
 			ctx.fillStyle = "rgba(75, 0, 0, 1)";
 			ctx.fillRect(0, 0, canvas.width, canvas.height);
-			coinToss();
+			coinChoose();
 		}
 	});
 });
 
-// Determines who bets first
-function coinToss() {
+// Player chooses heads or tails
+function coinChoose() {
 	// Text for UI
 	ctx.fillStyle = "Gold";
 	ctx.font = "40px Helvetica";
@@ -81,19 +84,57 @@ function coinToss() {
 	
 	// If heads is chosen
 	$("#heads").click(function(){
+		toss = "heads";
 		$("#heads").css("visibility", "hidden");
 		$("#tails").css("visibility", "hidden");
-		startGame();
+		coinToss();
 	});
 	
 	// If tails is chosen
 	$("#tails").click(function(){
+		toss = "tails";
 		$("#heads").css("visibility", "hidden");
 		$("#tails").css("visibility", "hidden");
-		startGame();
+		coinToss();
 	});
 }
 
+// The coin toss itself
+function coinToss() {
+	coin = Math.floor(Math.random() * 2);
+	
+	// Heads
+	if (coin === 0) {
+		if (toss === "heads") {
+			userBet = true;
+			$("#tossResult p").html("The coin is heads. You bet first.");
+		}
+		else {
+			$("#tossResult p").html("The coin is heads. The AI bets first.");
+		}
+	}
+	
+	// Tails
+	else {
+		$("#tossResult").css("background-image", "url('../images/coinTail.png')");
+		if (toss === "tails") {
+			userBet = true;
+			$("#tossResult p").html("The coin is tails. You bet first.");
+		}
+		else {
+			$("#tossResult p").html("The coin is tails. The AI bets first.");
+		}
+	}
+	
+	// We can see the result
+	$("#tossResult").css("visibility", "visible");
+	$("#tossResult").fadeOut(3000);
+	
+	// Begin game
+	startGame();
+}
+
+// Loads game screen
 function startGame() {
 	// Setting background color
 	ctx.fillStyle = "rgba(75, 0, 0, 1)";
@@ -157,66 +198,3 @@ function startGame() {
 	ctx.fillStyle = "#FF8C00";
 	ctx.fillText("VS.", canvas.width/2, canvas.height/2);
 }
-
-/*
-// Setting background color
-ctx.fillStyle = "rgba(75, 0, 0, 1)";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-// Creating card containers
-ctx.fillStyle = "Gold";
-ctx.fillRect(24, 49, 952, 252);
-ctx.fillRect(24, 599, 952, 252);
-ctx.fillRect(284, 324, 177, 252);
-ctx.fillRect(539, 324, 177, 252);
-ctx.fillStyle = "rgba(0, 0, 0, 1)";
-ctx.fillRect(25, 50, 950, 250);
-ctx.fillRect(25, 600, 950, 250);
-ctx.fillRect(285, 325, 175, 250);
-ctx.fillRect(540, 325, 175, 250);
-ctx.fillStyle = "Gold";
-ctx.fillRect(39, 89, 142, 202);
-ctx.fillRect(194, 89, 142, 202);
-ctx.fillRect(349, 89, 142, 202);
-ctx.fillRect(506, 89, 142, 202);
-ctx.fillRect(661, 89, 142, 202);
-ctx.fillRect(816, 89, 142, 202);
-ctx.fillRect(39, 639, 142, 202);
-ctx.fillRect(194, 639, 142, 202);
-ctx.fillRect(349, 639, 142, 202);
-ctx.fillRect(506, 639, 142, 202);
-ctx.fillRect(661, 639, 142, 202);
-ctx.fillRect(816, 639, 142, 202);
-ctx.fillStyle = "rgba(0, 0, 0, 1)";
-ctx.fillRect(40, 90, 140, 200);
-ctx.fillRect(195, 90, 140, 200);
-ctx.fillRect(350, 90, 140, 200);
-ctx.fillRect(507, 90, 140, 200);
-ctx.fillRect(662, 90, 140, 200);
-ctx.fillRect(817, 90, 140, 200);
-ctx.fillRect(40, 640, 140, 200);
-ctx.fillRect(195, 640, 140, 200);
-ctx.fillRect(350, 640, 140, 200);
-ctx.fillRect(507, 640, 140, 200);
-ctx.fillRect(662, 640, 140, 200);
-ctx.fillRect(817, 640, 140, 200);
-
-// Creating Health and Name indicators
-ctx.font = "20px Helvetica";
-ctx.fillStyle = "white";
-ctx.fillText("Your HP:", 25, 880);
-ctx.fillText("Their HP:", 25, 30);
-ctx.fillText("Their Name:", 300, 30);
-ctx.fillText("Your Name:", 300, 880);
-
-// Creating card text
-ctx.font = "20px Courier";
-ctx.textAlign = "center";
-ctx.fillText("THEIR HAND", canvas.width/2, 75);
-ctx.fillText("YOUR HAND", canvas.width/2, 625);
-
-// Versus logo
-ctx.font = "40px Impact";
-ctx.fillStyle = "#FF8C00";
-ctx.fillText("VS.", canvas.width/2, canvas.height/2);
-*/
